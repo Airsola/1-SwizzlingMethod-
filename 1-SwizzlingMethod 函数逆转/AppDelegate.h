6035054a33c37032693d8c29ebee0deda224a8d5
ccguo <389878212@qq.com>
2015-07-16 17:32:35 +0800

    Initial Commit

---
+++ 1-SwizzlingMethod 函数逆转/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1-SwizzlingMethod 函数逆转
+//
+//  Created by summer on 15/7/16.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1-SwizzlingMethod 函数逆转/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  1-SwizzlingMethod 函数逆转
+//
+//  Created by summer on 15/7/16.
+//  Copyright (c) 2015年 summer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
